c3025a19b985758259b4fae823de791a9dd20bc6
Fbada006 <ferdinandkamata@yahoo.com>
2020-05-07 21:51:05 +0300

    task: add extension function to handle back button on toolbar

--- app/src/main/java/com/disruption/moveery/utils/Extensions.kt
+++ app/src/main/java/com/disruption/moveery/utils/Extensions.kt
@@ -1,11 +1,24 @@
 package com.disruption.moveery.utils
 
 import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentActivity
 import com.bumptech.glide.RequestManager
+import com.disruption.moveery.R
 
 /**Extension function on [ImageView] to load images with [Glide]*/
 fun ImageView.loadImage(url: String, requestManager: RequestManager) {
     requestManager
         .load(url)
         .into(this)
+}
+
+/**Show and handle the back button for fragments*/
+fun Toolbar.showAndHandleBackButton(activity: FragmentActivity?) {
+    this.apply {
+        setNavigationIcon(R.drawable.ic_arrow_back)
+        setNavigationOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
 }
