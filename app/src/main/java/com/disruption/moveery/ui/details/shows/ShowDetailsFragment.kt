5c11e2f244806d3bb5be5a58ec671f3a88343697
Fbada006 <ferdinandkamata@yahoo.com>
2020-05-07 21:41:21 +0300

    task: display data in ShowDetailsFragment

--- app/src/main/java/com/disruption/moveery/ui/details/shows/ShowDetailsFragment.kt
+++ app/src/main/java/com/disruption/moveery/ui/details/shows/ShowDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,11 +13,15 @@ import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.RequestManager
 import com.disruption.moveery.R
 import com.disruption.moveery.databinding.ShowDetailsFragmentBinding
+import com.disruption.moveery.di.Injectable
 import com.disruption.moveery.models.shows.TvShow
+import com.disruption.moveery.utils.Constants
+import com.disruption.moveery.utils.DetailsHelper
+import com.disruption.moveery.utils.loadImage
 import javax.inject.Inject
 
 /**Fragment to show details of a clicked [TvShow]*/
-class ShowDetailsFragment : Fragment() {
+class ShowDetailsFragment : Fragment(), Injectable {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -42,23 +47,28 @@ class ShowDetailsFragment : Fragment() {
         return binding.root
     }
 
-//    /**Display the passed in movie from the [args]*/
-//    private fun displayShowDetails(tvShow: TvShow?) {
-//        val posterUrl = Constants.IMAGE_BASE_URL + tvShow?.poster_path
-//
-//        setMoviePoster(posterUrl)
-//
-//        val average = ((tvShow?.vote_average)!! * 10).toInt()
-//
-//        binding.tvMovieTitle.text = tvShow.original_title
-//        binding.tvMovieGenre.text = DetailsHelper.getGenres(tvShow.genre_ids, requireContext())
-//        binding.tvMovieYear.text = tvShow.release_date?.substring(0, 4)
-//        binding.tvMovieOverview.text = tvShow.overview
-//        binding.tvMovieRating.text = average.toString().plus("%")
-//        binding.ratingCustomView.apply {
-//            setValue(average)
-//            setFillColor(DetailsHelper.getRatingColor(average, requireContext()))
-//            setStrokeColor(ContextCompat.getColor(requireContext(), R.color.colorAccent))
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (args.tvshow != null) displayShowDetails(args.tvshow)
+    }
+
+    /**Display the passed in movie from the [args]*/
+    private fun displayShowDetails(tvShow: TvShow?) {
+        val posterUrl = Constants.IMAGE_BASE_URL + tvShow?.poster_path
+
+        binding.ivShowPoster.loadImage(posterUrl, requestManager)
+
+        val average = ((tvShow?.vote_average)!! * 10).toInt()
+
+        binding.tvShowTitle.text = tvShow.name
+        binding.tvShowGenre.text = DetailsHelper.getGenres(tvShow.genre_ids, requireContext())
+        binding.tvShowYear.text = tvShow.first_air_date?.substring(0, 4)
+        binding.tvShowOverview.text = tvShow.overview
+        binding.tvShowRating.text = average.toString().plus("%")
+        binding.ratingCustomView.apply {
+            setValue(average)
+            setFillColor(DetailsHelper.getRatingColor(average, requireContext()))
+            setStrokeColor(ContextCompat.getColor(requireContext(), R.color.colorAccent))
+        }
+    }
 }
