8ce4f644ff8f6435f0402b3126a99e92111d29ef
Fbada006 <ferdinandkamata@yahoo.com>
2020-05-08 17:26:33 +0300

    doc: add code documentation

--- app/src/main/java/com/disruption/moveery/di/AssisstedInjectModule.kt
+++ app/src/main/java/com/disruption/moveery/di/AssisstedInjectModule.kt
@@ -1,6 +1,5 @@
 package com.disruption.moveery.di
 
-import com.disruption.moveery.AssistedInject_AssistedInjectModule
 import com.squareup.inject.assisted.dagger2.AssistedModule
 import dagger.Module
 
