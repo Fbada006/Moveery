ba3d3866d19cf2aca82eb1f6c9bbc21424096d17
Fbada006 <ferdinandkamata@yahoo.com>
2020-05-20 10:49:48 +0300

    task: add testing for DetailsHelper

---
+++ app/src/androidTest/java/com/disruption/moveery/utils/DetailsHelperTest.kt
@@ -0,0 +1,91 @@
+package com.disruption.moveery.utils
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.disruption.moveery.R
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class DetailsHelperTest {
+
+    /**Test that the genre strings are properly returned*/
+    @Test
+    fun getGenres_actionDocumentaryDrama_returnsActionDocumentaryDrama() {
+        //Given a list of the following genres
+        val genreIds = listOf(28, 99, 18)
+
+        //When getGenres() is called
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val genres = DetailsHelper.getGenres(genreIds, context)
+
+        //Then assert that the string is "Action, Documentary, Drama."
+        assertThat(genres).isEqualTo("Action,Documentary,Drama.")
+    }
+
+    /**Test that the no genre string is returned given an empty list*/
+    @Test
+    fun getGenres_actionEmptyList_returnsNoGenreProvided() {
+        //Given an empty list of genres
+        val genreIds = emptyList<Int>()
+
+        //When getGenres() is called
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val genres = DetailsHelper.getGenres(genreIds, context)
+
+        //Then assert that the string is "Genre Unknown"
+        assertThat(genres).isEqualTo("Genre Unknown.")
+    }
+
+    /**Test that the no genre string is returned given a null list*/
+    @Test
+    fun getGenres_actionNullList_returnsNoGenreProvided() {
+        //Given an empty list of genres
+        val genreIds = null
+
+        //When getGenres() is called
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val genres = DetailsHelper.getGenres(genreIds, context)
+
+        //Then assert that the string is "Genre Unknown"
+        assertThat(genres).isEqualTo("Genre Unknown.")
+    }
+
+    /**Test that the lessThan100OrEqual color is returned*/
+    @Test
+    fun getRatingColor_action80Rating_returnsLessThan100OrEqual() {
+        //Given a rating of 80
+        val rating = 80
+
+        //When getRatingColor() is called
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val ratingColor = DetailsHelper.getRatingColor(rating, context)
+
+        //Then assert that the color is lessThan100OrEqual
+        assertThat(ratingColor).isEqualTo(
+            ContextCompat.getColor(
+                context,
+                R.color.lessThan100OrEqual
+            )
+        )
+    }
+
+    /**Test that the accent color is returned*/
+    @Test
+    fun getRatingColor_action1000Rating_returnsColorAccent() {
+        //Given a rating of 1000
+        val rating = 1000
+
+        //When getRatingColor() is called
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val ratingColor = DetailsHelper.getRatingColor(rating, context)
+
+        //Then assert that the color is colorAccent
+        assertThat(ratingColor).isEqualTo(ContextCompat.getColor(context, R.color.colorAccent))
+    }
+}
--- app/src/main/java/com/disruption/moveery/utils/DetailsHelper.kt
+++ app/src/main/java/com/disruption/moveery/utils/DetailsHelper.kt
@@ -7,29 +7,33 @@ import com.disruption.moveery.R
 /**Handles getting the genres of the selected movie*/
 object DetailsHelper {
     /**Returns the genres of the movie from the given [genreIds]*/
-    fun getGenres(genreIds: List<Int>?, context: Context): String {
+    fun getGenres(genreIds: List<Int>?, context: Context?): String {
+        if (genreIds.isNullOrEmpty()) return context?.getString(R.string.genre_unknown)!!
+            .trimEnd(',', ' ').plus(".")
+
         val stringBuilder = StringBuilder()
-        genreIds?.forEach {
+        genreIds.forEach {
             when (it) {
-                28 -> stringBuilder.append(context.getString(R.string.genre_action))
-                12 -> stringBuilder.append(context.getString(R.string.genre_adventure))
-                16 -> stringBuilder.append(context.getString(R.string.genre_animation))
-                35 -> stringBuilder.append(context.getString(R.string.genre_comedy))
-                80 -> stringBuilder.append(context.getString(R.string.genre_crime))
-                99 -> stringBuilder.append(context.getString(R.string.genre_documentary))
-                18 -> stringBuilder.append(context.getString(R.string.genre_drama))
-                10751 -> stringBuilder.append(context.getString(R.string.genre_family))
-                14 -> stringBuilder.append(context.getString(R.string.genre_fantasy))
-                36 -> stringBuilder.append(context.getString(R.string.genre_history))
-                27 -> stringBuilder.append(context.getString(R.string.genre_horror))
-                10402 -> stringBuilder.append(context.getString(R.string.genre_music))
-                9648 -> stringBuilder.append(context.getString(R.string.genre_mystery))
-                10749 -> stringBuilder.append(context.getString(R.string.genre_romance))
-                878 -> stringBuilder.append(context.getString(R.string.genre_scifi))
-                10770 -> stringBuilder.append(context.getString(R.string.genre_tv_movie))
-                53 -> stringBuilder.append(context.getString(R.string.genre_thriller))
-                10752 -> stringBuilder.append(context.getString(R.string.genre_war))
-                37 -> stringBuilder.append(context.getString(R.string.genre_western))
+                28 -> stringBuilder.append(context?.getString(R.string.genre_action))
+                12 -> stringBuilder.append(context?.getString(R.string.genre_adventure))
+                16 -> stringBuilder.append(context?.getString(R.string.genre_animation))
+                35 -> stringBuilder.append(context?.getString(R.string.genre_comedy))
+                80 -> stringBuilder.append(context?.getString(R.string.genre_crime))
+                99 -> stringBuilder.append(context?.getString(R.string.genre_documentary))
+                18 -> stringBuilder.append(context?.getString(R.string.genre_drama))
+                10751 -> stringBuilder.append(context?.getString(R.string.genre_family))
+                14 -> stringBuilder.append(context?.getString(R.string.genre_fantasy))
+                36 -> stringBuilder.append(context?.getString(R.string.genre_history))
+                27 -> stringBuilder.append(context?.getString(R.string.genre_horror))
+                10402 -> stringBuilder.append(context?.getString(R.string.genre_music))
+                9648 -> stringBuilder.append(context?.getString(R.string.genre_mystery))
+                10749 -> stringBuilder.append(context?.getString(R.string.genre_romance))
+                878 -> stringBuilder.append(context?.getString(R.string.genre_scifi))
+                10770 -> stringBuilder.append(context?.getString(R.string.genre_tv_movie))
+                53 -> stringBuilder.append(context?.getString(R.string.genre_thriller))
+                10752 -> stringBuilder.append(context?.getString(R.string.genre_war))
+                37 -> stringBuilder.append(context?.getString(R.string.genre_western))
+                else -> stringBuilder.append(context?.getString(R.string.genre_unknown))
             }
         }
 
