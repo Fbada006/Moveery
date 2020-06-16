6b7357cade425eed498f08d038177296aca4347c
Fbada006 <ferdinandkamata@yahoo.com>
2020-06-16 20:57:10 +0300

    -update build.gradle

--- app/src/main/java/com/droidafricana/moveery/utils/Constants.kt
+++ app/src/main/java/com/droidafricana/moveery/utils/Constants.kt
@@ -1,9 +1,11 @@
 package com.droidafricana.moveery.utils
 
+import com.droidafricana.moveery.BuildConfig
+
 /**Helper object for app constants*/
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/"
-    const val STRAW = "9ed751698b8b5963444886c285a5afb1"
+    const val STRAW = BuildConfig.BingerSecretKey
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
     const val YOUTUBE_IMAGE_BASE_URL = "https://img.youtube.com/vi/%s/mqdefault.jpg"
     const val YOUTUBE_VIDEO_BASE_URL = "http://www.youtube.com/watch?v=%s"
