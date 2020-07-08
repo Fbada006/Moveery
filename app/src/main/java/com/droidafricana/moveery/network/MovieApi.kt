b7e3b4f30d66049fc0879881c9f74ee9effd549b
Fbada006 <ferdinandkamata@yahoo.com>
2020-07-08 22:57:00 +0300

    task: remove logging

--- app/src/main/java/com/droidafricana/moveery/network/MovieApi.kt
+++ app/src/main/java/com/droidafricana/moveery/network/MovieApi.kt
@@ -3,22 +3,21 @@ package com.droidafricana.moveery.network
 import com.droidafricana.moveery.utils.Constants
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 /**OkHttp for basic retrofit setup*/
 //TODO: Get rid of this logging
-val interceptor: HttpLoggingInterceptor =
-    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+//val interceptor: HttpLoggingInterceptor =
+//    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
 
 /**The client to set up retrofit*/
 val client = OkHttpClient.Builder()
     .callTimeout(30, TimeUnit.SECONDS)
     .connectTimeout(30, TimeUnit.SECONDS)
     .readTimeout(30, TimeUnit.SECONDS)
-    .addInterceptor(interceptor)
+    // .addInterceptor(interceptor)
     .retryOnConnectionFailure(true)
     .writeTimeout(30, TimeUnit.SECONDS)
     .build()
