06060db31f2b72d19851a5b745e6dbf69cdcd2b2
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-28 18:46:57 +0300

    add logic to the similar viewModels

--- app/src/main/java/com/droidafricana/moveery/ui/details/similardetails/similarmovie/SimilarMovieDetailsViewModel.kt
+++ app/src/main/java/com/droidafricana/moveery/ui/details/similardetails/similarmovie/SimilarMovieDetailsViewModel.kt
@@ -1,8 +1,58 @@
 package com.droidafricana.moveery.ui.details.similardetails.similarmovie
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.droidafricana.moveery.models.movies.Movie
+import com.droidafricana.moveery.models.videos.Video
+import com.droidafricana.moveery.repo.MovieRepo
+import com.droidafricana.moveery.utils.Constants
+import com.droidafricana.moveery.utils.Event
+import com.droidafricana.moveery.utils.Resource
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SimilarMovieDetailsViewModel @Inject constructor() : ViewModel() {
+class SimilarMovieDetailsViewModel @Inject constructor(private val repo: MovieRepo) :
+    ViewModel() {
 
+    /*The internal MutableLiveData that stores the event of a click input */
+    private val _navigateToSelectedMovie = MutableLiveData<Event<Movie>>()
+
+    /**The external immutable LiveData for the click event*/
+    val navigateToSelectedMovie: LiveData<Event<Movie>>
+        get() = _navigateToSelectedMovie
+
+    private val _videoRes = MutableLiveData<Resource<List<Video>>>()
+
+    /**The resource that will supply the videos*/
+    val videoRes: LiveData<Resource<List<Video>>>
+        get() = _videoRes
+
+    /**Method that gets the videos*/
+    fun getVideosResource(id: Int) {
+        viewModelScope.launch {
+            _videoRes.value = repo.getVideos(Constants.MOVIE_TYPE, id).value
+        }
+    }
+
+    /**Insert a movie to favourites*/
+    fun insertMovieIntoFav(movie: Movie) {
+        viewModelScope.launch { repo.insertMovieToFav(movie) }
+    }
+
+    /**Delete a movie from favourites*/
+    fun deleteMoveFromFav(id: Int) {
+        viewModelScope.launch { repo.deleteMovieFromFav(id) }
+    }
+
+    /**If the movie is null, it is not in fav and so the like button should be in the un-liked state
+     * and vice-versa
+     **/
+    fun isMovieInFav(id: Int) = repo.getMovieById(id)
+
+    /**Called when a user clicks on a movie*/
+    fun displayMovieDetails(movie: Movie) {
+        _navigateToSelectedMovie.value = Event(movie)
+    }
 }
--- app/src/main/java/com/droidafricana/moveery/ui/details/similardetails/similarshow/SimilarShowsViewModel.kt
+++ app/src/main/java/com/droidafricana/moveery/ui/details/similardetails/similarshow/SimilarShowsViewModel.kt
@@ -1,8 +1,58 @@
 package com.droidafricana.moveery.ui.details.similardetails.similarshow
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.droidafricana.moveery.models.shows.TvShow
+import com.droidafricana.moveery.models.videos.Video
+import com.droidafricana.moveery.repo.MovieRepo
+import com.droidafricana.moveery.utils.Constants
+import com.droidafricana.moveery.utils.Event
+import com.droidafricana.moveery.utils.Resource
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SimilarShowsViewModel @Inject constructor() : ViewModel() {
+class SimilarShowsViewModel @Inject constructor(private val repo: MovieRepo) :
+    ViewModel() {
 
+    /* The internal MutableLiveData that stores the event of a click input */
+    private val _navigateToSelectedShow = MutableLiveData<Event<TvShow>>()
+
+    /**The external immutable LiveData for the click event*/
+    val navigateToSelectedShow: LiveData<Event<TvShow>>
+        get() = _navigateToSelectedShow
+
+    private val _videoRes = MutableLiveData<Resource<List<Video>>>()
+
+    /**The resource that will supply the videos*/
+    val videoRes: LiveData<Resource<List<Video>>>
+        get() = _videoRes
+
+    /**Method that gets the videos*/
+    fun getVideosResource(id: Int) {
+        viewModelScope.launch {
+            _videoRes.value = repo.getVideos(Constants.SHOW_TYPE, id).value
+        }
+    }
+
+    /**Insert a show to favourites*/
+    fun insertShowIntoFav(show: TvShow) {
+        viewModelScope.launch { repo.insertShowToFav(show) }
+    }
+
+    /**Delete a show from favourites*/
+    fun deleteShowFromFav(id: Int) {
+        viewModelScope.launch { repo.deleteShowFromFav(id) }
+    }
+
+    /**If the show is null, it is not in fav and so the like button should be in the un-liked state
+     * and vice-versa
+     */
+    fun isShowInFav(id: Int) = repo.getShowById(id)
+
+    /**Called when a user clicks on a show*/
+    fun displayShowDetails(show: TvShow) {
+        _navigateToSelectedShow.value = Event(show)
+    }
 }
